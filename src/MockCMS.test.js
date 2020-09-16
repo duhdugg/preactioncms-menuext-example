8741a9d4d23facd26608b3d76024f0a6d38fed59
Doug Elkin <doug@dougelkin.com>
2020-09-15 21:58:44 -0400

    :white_check_mark: updated test

--- src/MockCMS.test.js
+++ src/MockCMS.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import MockCMS from './MockCMS'
 
-test('renders learn react link', () => {
+test('renders', () => {
   const result = render(<MockCMS />)
   expect(result).toBeTruthy()
 })
